058f286d3bd036ba80daad5fc3a22ef7fdea1aaa
soichi-fujiwara <52342154+soichi-fujiwara@users.noreply.github.com>
2019-07-23 22:49:07 +0900

    Update SalesMail.py

--- SalesMail.py
+++ SalesMail.py
@@ -1,71 +1,71 @@
 import re
 import mojimoji
+import MeCab
+import pandas as pd
 
 def tanka_serialize(words):
-    #大文字統一
-    words = words.upper()
+  #大文字統一
+  words = words.upper()
 
-    #英数字のみ半角統一
-    words = mojimoji.zen_to_han(words, kana=False)
+  #英数字のみ半角統一
+  words = mojimoji.zen_to_han(words, kana=False)
       
-    #電話番号削除
-    #(稼働時間と形式が重複する為)
-    if len(re.findall('[0-9]{3}-[0-9]{4}-[0-9]{4}',words)) > 0:
-      tel_list = re.findall('[0-9]{3}-[0-9]{4}-[0-9]{4}',words)
-      if len(tel_list) > 0:
-        for tel in tel_list:
-          words = words.replace(tel,'')
+  #電話番号削除
+  #(稼働時間と形式が重複する為)
+  if len(re.findall('[0-9]{3}-[0-9]{4}-[0-9]{4}',words)) > 0:
+    tel_list = re.findall('[0-9]{3}-[0-9]{4}-[0-9]{4}',words)
+    if len(tel_list) > 0:
+      for tel in tel_list:
+        words = words.replace(tel,'')
 
-    #チルダ統一1
-    ser_word = ["〜","～", "-","－","から","MAX"]
-    for word in ser_word:
-      words = words.replace(word,"~")
+  #チルダ統一1
+  ser_word = ["〜","～", "-","－","から","MAX"]
+  for word in ser_word:
+    words = words.replace(word,"~")
       
-    #稼働時間削除
-    if len(re.findall('[0-9]{3}H~[0-9]{3}H',words)) > 0:
-      kado = re.findall('[0-9]{3}H~[0-9]{3}H',words)[0]
-      words = words.replace(kado,"")
-    if len(re.findall('[0-9]{3}~[0-9]{3}H',words)) > 0:
-      kado = re.findall('[0-9]{3}~[0-9]{3}H',words)[0]
-      words = words.replace(kado,"")
-    if len(re.findall('[0-9]{3}~[0-9]{3}',words)) > 0:
-      kado = re.findall('[0-9]{3}~[0-9]{3}',words)[0]
-      words = words.replace(kado,"")
+  #稼働時間削除
+  if len(re.findall('[0-9]{3}H~[0-9]{3}H',words)) > 0:
+    kado = re.findall('[0-9]{3}H~[0-9]{3}H',words)[0]
+    words = words.replace(kado,"")
+  if len(re.findall('[0-9]{3}~[0-9]{3}H',words)) > 0:
+    kado = re.findall('[0-9]{3}~[0-9]{3}H',words)[0]
+    words = words.replace(kado,"")
+  if len(re.findall('[0-9]{3}~[0-9]{3}',words)) > 0:
+    kado = re.findall('[0-9]{3}~[0-9]{3}',words)[0]
+    words = words.replace(kado,"")
       
-    #スキル見合い
-    ser_word = ["スキル見合い","スキル見合","ご相談","相談","ご提示下さい","ご提示ください","スキルにより相談"]
-    for word in ser_word:
-      words = words.replace(word,"")
+  #スキル見合い
+  ser_word = ["スキル見合い","スキル見合","ご相談","相談","ご提示下さい","ご提示ください","スキルにより相談"]
+  for word in ser_word:
+    words = words.replace(word,"")
       
-    #曖昧単価加工
-    if len(re.findall('[0-9]{1}・',words)) > 0:
-      aimai = re.findall('[0-9]{1}・',words)[0]
-      words = words.replace(aimai,"")
-    if len(re.findall('[0-9]{1}･',words)) > 0:
-      aimai = re.findall('[0-9]{1}･',words)[0]
-      words = words.replace(aimai,"")
-    if len(re.findall('[0-9]{1}、',words)) > 0:
-      aimai = re.findall('[0-9]{1}、',words)[0]
-      words = words.replace(aimai,"")
+  #曖昧単価加工
+  if len(re.findall('[0-9]{1}・',words)) > 0:
+    aimai = re.findall('[0-9]{1}・',words)[0]
+    words = words.replace(aimai,"")
+  if len(re.findall('[0-9]{1}･',words)) > 0:
+    aimai = re.findall('[0-9]{1}･',words)[0]
+    words = words.replace(aimai,"")
+  if len(re.findall('[0-9]{1}、',words)) > 0:
+    aimai = re.findall('[0-9]{1}、',words)[0]
+    words = words.replace(aimai,"")
       
-    #改行コード変換
-    words = words.replace("\r\n","")
+  #改行コード変換
+  words = words.replace("\r\n","")
 
-    #小数点無効
-    words = words.replace(".0","")
+  #小数点無効
+  words = words.replace(".0","")
 
-    #万円単位変換
-    words = words.replace("0000","")
+  #万円単位変換
+  words = words.replace("0000","")
     
-    #範囲指定正規化(「まで」表現置換)
-    #search(最初の一致文字列の数値のみを返す)
-    if re.search('[0-9]{2}まで',words):
-      made_val = re.sub(r'\D', '',re.search('[0-9]{2}まで',words).group())
-      words = words.replace(made_val+'まで','~'+made_val)
+  #範囲指定正規化(「まで」表現置換)
+  #search(最初の一致文字列の数値のみを返す)
+  if re.search('[0-9]{2}まで',words):
+    made_val = re.sub(r'\D', '',re.search('[0-9]{2}まで',words).group())
+    words = words.replace(made_val+'まで','~'+made_val)
       
-    return words
- 
-import MeCab
+  return words
 
 def tanka_pickup(str_wk):
   #======================================================
@@ -114,8 +114,6 @@ def tanka_pickup(str_wk):
       tanka_str = "単価完全なし"
     
   return tanka_str 
-
-import pandas as pd
 
 def tanka_output(pickup_str): 
 
